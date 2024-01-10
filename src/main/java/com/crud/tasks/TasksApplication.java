ccf70921acc21a835fcb0e808ee585aa5e5acdd1
Michał Zimny <mzmichalzimny@gmail.com>
2024-01-10 13:27:58 +0100

    19.4

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -4,9 +4,11 @@ import com.crud.tasks.domain.TaskDto;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @OpenAPIDefinition
 @SpringBootApplication
+@EnableScheduling
 public class TasksApplication {
 
 	public static void main(String[] args) {

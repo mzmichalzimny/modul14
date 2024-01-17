896c537e6571f44ac21dfd6a30885b3da4cca6fd
Michał Zimny <mzmichalzimny@gmail.com>
2024-01-17 13:08:49 +0100

    24.3

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -17,13 +20,32 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/crud");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("goodbye_message", "Twoja wiadomość pożegnalna");
-        context.setVariable("company_details", adminConfig.getCompanyName());
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String buildDailyTasksEmail(int taskCount) {
+        Context context = new Context();
+        context.setVariable("message", "Masz " + taskCount + " zadania.");
+        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("button", "Zobacz zadania");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("preview_message", "Twoje codzienne zadania");
+        context.setVariable("goodbye_message", "Miłego dnia!");
+        context.setVariable("company_details", adminConfig.getCompanyName());
+        return templateEngine.process("mail/daily-tasks-mail", context);
     }
 }
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -1,11 +1,11 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
@@ -15,9 +15,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class SimpleEmailService {
-@Autowired
+    @Autowired
     private final JavaMailSender javaMailSender;
+    @Autowired
     private final MailCreatorService mailCreatorService;
+    @Autowired
+    private final AdminConfig adminConfig;
 
     public void send(final Mail mail) {
         log.info("Starting email preparation...");
@@ -37,5 +40,14 @@ public class SimpleEmailService {
             messageHelper.setSubject(mail.getSubject());
             messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
         };
+    }
+
+    public void sendDailyTasksEmail(int taskCount) {
+        Mail mail = new Mail(
+                adminConfig.getAdminMail(),
+                "Daily tasks update",
+                "You have " + taskCount + " tasks.", "a"
+        );
+        send(mail);
     }
 }
